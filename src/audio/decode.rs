e9d658a3149ebe3bdaf6768c19ba62a5c34f32cb
homepunks <wispenjoyer@gmail.com>
2026-06-27 16:53:15 +0500

    feat: decode opus packets

--- src/audio/decode.rs
+++ src/audio/decode.rs
@@ -1,4 +1,5 @@
 use std::io::Cursor;
+use opus::{Channels, Decoder};
 
 fn demux_ogg(audio_raw: &[u8]) -> anyhow::Result<Vec<Vec<u8>>> {
     let cursor = Cursor::new(audio_raw);
@@ -12,6 +13,25 @@ fn demux_ogg(audio_raw: &[u8]) -> anyhow::Result<Vec<Vec<u8>>> {
     Ok(packets)
 }
 
-// fn decode_opus_packets(packets: &[Vec<u8>]) -> anyhow::Result<Vec<f32>> {
-//
-// }
+fn decode_opus_packets(packets: &[Vec<u8>]) -> anyhow::Result<Vec<f32>> {
+    let mut decoder = Decoder::new(48_000, Channels::Mono)?;
+    let mut pcm = Vec::new();
+    let mut frame = [0f32; 5760]; /* 48k * .12 */
+
+    for p in packets {
+        if p.starts_with(b"OpusHead") || p.starts_with(b"OpusTags") {
+            continue;
+        }
+
+        let n = decoder.decode_float(p, &mut frame, false)?;
+        pcm.extend_from_slice(&frame[..n]);
+    }
+
+    Ok(pcm)
+}
+
+pub fn decode_voice(audio_raw: &[u8]) -> anyhow::Result<Vec<f32>> {
+    let packets = demux_ogg(audio_raw)?;
+    let pcm = decode_opus_packets(&packets)?;
+    Ok(pcm)
+}
